a9ef4cfe66a5e38abd70d5e70a8b40fec57e25c5
Tugi-Ngunjiri <blueqwinn@gmail.com>
2023-11-06 08:15:26 +0300

    :lipstick: I have added more  styling to my features.css

--- src/Components/Features.tsx
+++ src/Components/Features.tsx
@@ -15,16 +15,18 @@ function Features() {
    ];
   return (
     <nav>
-      <div >
-      <img src= "https://res.cloudinary.com/dyl3rncv3/image/upload/v1679650530/elewa-team_digqu3.jpg"  alt="Image"></img>
-      </div>
      <div>
+    <div  >
+      <img src= "https://res.cloudinary.com/dyl3rncv3/image/upload/v1679650530/elewa-team_digqu3.jpg"  alt="Image"  >
+        
+      </img>
+      </div>
+    <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1679982347/elewa-group-website/hero-Images/dark-to-light_xrat5g.jpg"  alt="Image"></img>
+    </div>
+    <div>
       <img src= "https://res.cloudinary.com/dyl3rncv3/image/upload/v1679910469/elewa-group-website/hero-Images/pexels-bg-edit_bb4znx.jpg"  alt="Image"></img>
       </div>
      
-    <div>
-    <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1679982347/elewa-group-website/hero-Images/dark-to-light_xrat5g.jpg"  alt="Image"></img>
-    </div>
       <div className="dropdown">
     <button  className="dropbtn">Dropdown</button>
       </div>
